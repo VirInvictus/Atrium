7f9c4058c70d6d9aa07fdf766d8a69fb6404de06
Brandon LaRocque <larocque.brandon@gmail.com>
2026-08-24 00:20:18 -0400

    chore: remove accidental python patch script fix_domain.py

--- fix_domain.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open("atrium-core/src/search/domain.rs", "r") as f:
-    text = f.read()
-
-text = text.replace("match s.to_ascii_lowercase().as_str()", "match name.to_ascii_lowercase().as_str()")
-
-with open("atrium-core/src/search/domain.rs", "w") as f:
-    f.write(text)
